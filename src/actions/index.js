33121bf6cd578a7b29a634be4194402ed6f5ef94
marikomands <marikomands@gmail.com>
2022-03-04 14:33:58 +0000

    used Redux-Thunk to use API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
 import jsonPlaceholder from "../apis/jasonPlaceholder";
 
-export const fetchPost = () => {
-  return { type: "FETCH_POSTS" };
+// create inner function for API hit
+export const fetchPost = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/post");
 };
+dispatch({ type: "FETCH_POSTS", payload: response });
+
+// could be written as below:
+
+// export const fetchPost = () => {
+//   return async function (dispatch) {
+//     const promise = await jsonPlaceholder.get("/post");
+//   };
+//   dispatch({ type: "FETCH_POSTS", payload: response });
+// };
