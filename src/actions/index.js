3654e69d6bcfb86f541c4ae37daec5b55501fd27
marikomands <marikomands@gmail.com>
2022-03-16 11:20:15 +0000

    coment for Redux-Thunk and Async Await

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,30 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// action creator inside action creator:  need to dispatch manually again
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
   console.log(getState().posts);
 
   const userIds = _.uniq(_.map(getState().posts, "userId"));
+  console.log("🚀 ~ fetchPostsAndUsers ~ userIds", userIds);
   userIds.forEach((id) => dispatch(fetchUser(id)));
+  console.log("🚀 ~ fetchPostsAndUsers ~ userIds", userIds);
 };
 
-// create inner function for API hit
+// create inner function for API hit with Redux-Thunk
+// Redux-Thunk required for async await      not for promise but Redux process will be finished sonner than promise and not to be used.
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
   // console.log(response.data);
 };
 
+//users情報の取得
 export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-  // console.log(response.data);
+  console.log(response.data);
 };
 
 // export const fetchUser = (id) => async (dispatch) => {
